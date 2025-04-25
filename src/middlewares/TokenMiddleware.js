b77ad15d0a944932fdd97029398197db5ac1caa7
Mahesh Chudasama <mahesh.tzinfotech@gmail.com>
2025-04-25 20:07:34 +0530

    token in add  try

--- src/middlewares/TokenMiddleware.js
+++ src/middlewares/TokenMiddleware.js
@@ -12,49 +12,50 @@ const OrgUsersModel = db.OrgUsersModel;
 
 
 module.exports = async (req, res, next) => {
-    const token = DevelopMood ? req.headers['authorization'] : await decrypt(req.headers['authorization']);
+	try {
+		const token = DevelopMood ? req.headers["authorization"] : await decrypt(req.headers["authorization"]);
 
-    if (token !== undefined) {
-        jwt.verify(token, secureKey, async (err, decoded) => {
+		if (token !== undefined) {
+			jwt.verify(token, secureKey, async (err, decoded) => {
+				if (err) {
+					res.status(TOKEN_NOT_VALID_CODE).json({
+						status: false,
+						message: "Token is not valid!",
+					});
+					throw err;
+				} else {
+					UserModel.findByPk(decoded.UserId).then(async (data) => {
+						if (data) {
+							const userDetails = await OrgUsersModel.findOne({
+								attributes: [
+									"OrgUserId",
+									"OrgId",
+									"BranchId",
+									"UserId",
+									"RoleId",
+									"DefaultOrg",
+									[Sequelize.literal(`(SELECT RoleName FROM orgusers JOIN roles ON roles.RoleId = orgusers.RoleId LIMIT 1)`), "RoleName"],
+								],
+								where: {
+									UserId: decoded.UserId,
+									DefaultOrg: true,
+								},
+								raw: true,
+							});
 
-            if (err) {
-                res.status(TOKEN_NOT_VALID_CODE).json({
-                    status: false,
-                    message: "Token is not valid!"
-                })
-                throw err;
-            }
-            else {
-                UserModel.findByPk(decoded.UserId).then(async (data) => {
-                    if (data) {
-
-                        const userDetails = await OrgUsersModel.findOne({
-                            attributes: [
-                                "OrgUserId",
-                                "OrgId",
-                                "BranchId",
-                                "UserId",
-                                "RoleId",
-                                "DefaultOrg",
-                                [Sequelize.literal(`(SELECT RoleName FROM orgusers JOIN roles ON roles.RoleId = orgusers.RoleId LIMIT 1)`),"RoleName",]
-                            ],
-                            where: {
-                                UserId: decoded.UserId,
-                                DefaultOrg: true
-                            },
-                            raw: true
-                        });
-
-                        req.user = { ...data?.dataValues, ...userDetails };
-                        next();
-                        return;
-                    };
-                    res.status(TOKEN_NOT_PROVIDED_CODE).send({ status: false, message: 'Unauthorized!', });
-                })
-            }
-        })
-    }
-    else {
-        res.status(TOKEN_NOT_PROVIDED_CODE).json({ status: false, message: "No token provided!" })
-    };
+							req.user = {...data?.dataValues, ...userDetails};
+							next();
+							return;
+						}
+						res.status(TOKEN_NOT_PROVIDED_CODE).send({status: false, message: "Unauthorized!"});
+					});
+				}
+			});
+		} else {
+			res.status(TOKEN_NOT_PROVIDED_CODE).json({status: false, message: "No token provided!"});
+		}
+	} catch (error) {
+		console.log(`\x1b[91m ${error} \x1b[91m`);
+		res.status(TOKEN_NOT_PROVIDED_CODE).json({status: false, message: error.message});
+	}
 };
